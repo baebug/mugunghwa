222db1b9def67f43fbd205e98ff8eb9822f025b6
baebug <ufr134@gmail.com>
2021-05-27 15:24:43 +0900

    database

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,17 @@
-# from django.shortcuts import render
-
-# # Create your views here.
-
-# def post_list(request):
-#     return render(request, 'blog/post_list.html', {})
-
-
-
-
-
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Post
 
 
 
 def post_list(request):
-    template = loader.get_template('blog/post_list.html')
-    context = {
-    }
-    return HttpResponse(template.render(context, request))
+    # template = loader.get_template('blog/post_list.html')
+    # context = {
+    # }
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+    # return HttpResponse(template.render(context, request))
 
 def uikit(request):
     template = loader.get_template('blog/uikit.html')
