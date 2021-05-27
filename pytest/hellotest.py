6b75db3cedbf398d222c8352ca2b775715355030
baebug <ufr134@gmail.com>
2021-05-27 15:46:07 +0900

    database test

--- pytest/hellotest.py
+++ pytest/hellotest.py
@@ -1,4 +1,5 @@
 import pytest
+from blog.models import Post
 
 @pytest.fixture
 def first():
@@ -18,3 +19,17 @@ def test_context_fixture_order(second):
     print("In the test")
     assert True
     
+
+@pytest.fixture
+def setup_post():
+    Post.objects.create(title="test_title1", text="test_text1")
+    print("Set up second fixture")
+    yield
+    print("Clean up second fixture")
+
+@pytest.mark.django_db
+def test_post(setup_post):
+    test1 = Post.objects.get(title="test_title1")
+    print(test1)
+    assert test1.text == "test_text1"
+    assert Post.objects.all().count() == 1
